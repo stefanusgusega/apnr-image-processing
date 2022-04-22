0cd8194ff2a0dcb007f64fd41d92e8b97e183388
Stefanus Gusega Gunawan <gunawanstefanus006@gmail.com>
2022-04-22 11:07:53 +0700

    feat: tune the param of character segmentation

--- src/example.m
+++ src/example.m
@@ -1,49 +1,62 @@
-im = imread('../images/plat2.jpg');
+im = imread('../images/plat4.jpg');
 imgray = rgb2gray(im);
 imbin = imbinarize(imgray);
-imedge = edge(imgray, 'prewitt');
+imedge = edge(imgray, 'sobel');
 
-figure,imshow(im),title('im');
-figure,imshow(imgray),title('imgray');
-figure,imshow(imbin),title('imbin');
-figure,imshow(imedge),title('imedge');
+figure, imshow(im), title('im');
+figure, imshow(imgray), title('imgray');
+figure, imshow(imbin), title('imbin');
+figure, imshow(imedge), title('imedge');
 
 % Plate detection
-Iprops = regionprops(imedge, 'BoundingBox', 'Area','Image');
+Iprops = regionprops(imedge, 'BoundingBox', 'Area', 'Image');
 area = Iprops.Area;
 count = numel(Iprops);
 maxa = area;
 boundingBox = Iprops.BoundingBox;
-for i=1:count
+
+for i = 1:count
+
     if maxa < Iprops(i).Area
         maxa = Iprops(i).Area;
         boundingBox = Iprops(i).BoundingBox;
     end
+
 end
 
 % Crop the plate
-imcr = imcrop(imbin, boundingBox);
-figure, imshow(imcr), title('imcr');
+% imcr = imcrop(imbin, boundingBox);
+% figure, imshow(imcr), title('imcr');
 
-% Clean the small objects that have pixel area less than 500
-imclean = bwareaopen(~imbin, 500);
+% Get the size of the image
+[h, w] = size(imbin)
+
+% Clean the small objects that have pixel area less than 0.1% of image area
+imclean = bwareaopen(imbin, round(0.001 * h * w));
 figure, imshow(imclean), title('imclean');
 
-% figure,imshow(imbin),title('imbin');
+figure, imshow(~imbin), title('imbin');
 
 % Get regions/bounding boxes
-st = regionprops(imclean, 'BoundingBox', 'Area', 'Image');
+% If the white part dominates, then apply negative filter to make background black
+% Then apply connected component analysis and bounding box analysis onto it
+if bwarea(imclean) > bwarea(~imclean)
+    st = regionprops(~imclean, 'BoundingBox', 'Area', 'Image');
+else
+    st = regionprops(imclean, 'BoundingBox', 'Area', 'Image');
+end
 
 % Draw the box
-for k = 1: length(st)
+for k = 1:length(st)
     ow = length(st(k).Image(1, :))
     oh = length(st(k).Image(:, 1))
-    % If the area of rectangle more than 500 pixels, then draw the rect
-    if ow*oh >= 500
+    % The height of bounding box should be in range (0.2*h, 0.8*h)
+    % and its weight should be less than or equal with its height
+    if oh < (0.8 * h) && oh > (0.2 * h) && ow <= oh
         thisBB = st(k).BoundingBox;
-%     figure,imshow(imcrop(imcr, thisBB));
-        rectangle('Position', [thisBB(1), thisBB(2), thisBB(3), thisBB(4)],...
-        'EdgeColor','g','LineWidth',2)
+        %     figure,imshow(imcrop(imcr, thisBB));
+        rectangle('Position', [thisBB(1), thisBB(2), thisBB(3), thisBB(4)], ...
+        'EdgeColor', 'g', 'LineWidth', 2)
     end
-    
+
 end
